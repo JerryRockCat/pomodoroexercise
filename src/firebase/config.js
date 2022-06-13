09d92995dd19c5ac60a3dc8dd289ef86e25c2bd8
JerryRockCat <94103010+JerryRockCat@users.noreply.github.com>
2022-06-13 17:07:56 +0700

    *init file

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+//init firebase
+import 'firebase/firestore'
+
+import firebase from 'firebase/app'
+const firebaseConfig = {
+  apiKey: "AIzaSyCxCfGMCrsfXiWWoU7_0u3jvymQUQx4j5Y",
+  authDomain: "pomodoro-3c428.firebaseapp.com",
+  projectId: "pomodoro-3c428",
+  storageBucket: "pomodoro-3c428.appspot.com",
+  messagingSenderId: "556067863439",
+  appId: "1:556067863439:web:c067be930f50d8f905e9fa",
+  measurementId: "G-4NDTNTET0S"
+};
+
+firebase.initializeApp(firebaseConfig)
+
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    return (
+        <form>
+            <label htmlFor="email">
+                <span>Email: </span>
+            
+            </label>
+        
+        </form>
+    )
+}
